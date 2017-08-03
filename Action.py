e296bf64991a4534cd7f9f336d94e8bffd5b4475
vinhvu200 <vinhvu200@gmail.com>
2017-08-03 16:07:13 -0700

    Added help command, working on setting up db

--- Action.py
+++ Action.py
@@ -1,9 +1,17 @@
 import Lyrics
 import random
+import datetime
 
-prefix_1 = "Did you know..."
-prefix_2 = "Bet you didn't know..."
-interesting_prefix = [prefix_1, prefix_2]
+def help():
+
+    return 'Commands:\n' \
+              '\t!peter\n' \
+              '\t!brian\n' \
+              '\t!calvin\n' \
+              '\t!david\n' \
+              '\t!will\n' \
+              '\t!interesting\n' \
+              '\t!help'
 
 def peter():
     return Lyrics.Astley[random.randint(0, len(Lyrics.Astley)-1)]
@@ -18,7 +26,7 @@ def will():
     return Lyrics.CupCakke[random.randint(0, len(Lyrics.CupCakke)-1)]
 
 def david():
-    return Lyrics.Spongebob[random.randint(0, len(Lyrics.CupCakke)-1)]
+    return Lyrics.Spongebob[random.randint(0, len(Lyrics.Spongebob)-1)]
 
 def calvin():
     return Lyrics.Desiigner[random.randint(0, len(Lyrics.Desiigner)-1)]
@@ -27,8 +35,13 @@ def graham():
     return Lyrics.Hokey_pokey[random.randint(0, len(Lyrics.Hokey_pokey)-1)]
 
 def interesting(reddit):
+
     count = 0
     rand = random.randint(0, 29)
+    prefix_1 = "Did you know..."
+    prefix_2 = "Bet you didn't know..."
+    interesting_prefix = [prefix_1, prefix_2]
+
     subreddit = reddit.subreddit('TodayILearned').hot(limit=30)
 
     for submission in subreddit:
@@ -37,3 +50,19 @@ def interesting(reddit):
             return submission.title.replace('TIL', interesting_prefix[prefix_index])
             break
         count = count + 1
+
+def record(posts, message, author):
+
+    time = datetime.datetime.now()
+    new_time = time.strftime('%Y %m %d %H %M')
+    #reformatted_time = datetime.datetime.strptime(new_time, '%Y %m %d %H %M')
+
+    post_data = {
+        'content' : str(message),
+        'author' : str(author),
+        'time' : new_time
+    }
+
+    print(post_data)
+    result = posts.insert_one(post_data)
+    #print('One post: {0}'.format(result.inserted_id))
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import discord
-import asyncio
 import Action
 import praw
+import datetime
+from pymongo import MongoClient
 
+# Getting credentials
 f = open('Stuff.txt', 'r')
 token = f.readline()[:-1]
 client_id = f.readline()[:-1]
@@ -12,7 +14,14 @@ user_agent = 'stuff'
 username = f.readline()
 f.close()
 
+# Getting client
+reddit = praw.Reddit(client_id=client_id, client_secret=client_secret,
+                     password=password, user_agent=user_agent,
+                     username=username)
 client = discord.Client()
+client_2 = MongoClient()
+db = client_2.pymongo_test
+posts = db.posts
 
 @client.event
 async def on_ready():
@@ -27,7 +36,9 @@ async def on_message(message):
     content = message.content.lower()
     response = None
 
-    if content.startswith('!peter'):
+    if content.startswith('!help'):
+        response = Action.help()
+    elif content.startswith('!peter'):
         response = Action.peter()
     elif content.startswith('!brian'):
         response = Action.brian()
@@ -43,16 +54,17 @@ async def on_message(message):
         response = Action.graham()
     elif content.startswith('!interesting'):
         response = Action.interesting(reddit)
+    elif content.startswith('!record'):
+        Action.record(posts, content, message.author)
+    elif content.startswith('!pull'):
+        user = ''
+        vinhs_post = posts.find_one({'author': user})
+        time = vinhs_post['time']
+        print(time)
+        #reformatted_time = datetime.datetime.strptime(time, '%Y %m %d %H %M')
 
     if response is not None:
         tmp = await client.send_message(message.channel, 'Calculating messages...')
         await client.edit_message(tmp, response)
 
-    if message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
-
-reddit = praw.Reddit(client_id=client_id, client_secret=client_secret,
-                         password=password, user_agent=user_agent,
-                         username=username)
 client.run(token)
